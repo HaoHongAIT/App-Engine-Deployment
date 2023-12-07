9bea8ee32351767a42ce1341a4192495fc400109
hongtienhaolk2001 <hongtienhaolk2001@gmail.com>
2023-12-07 20:39:43 +0700

    231207

--- README.md
+++
@@ -1,2 +0,0 @@
-SQLAlchemy==2.0.23
-pymysql==1.1.0
--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 from .analysis import Analysis
+from .database import get_conn
 
-
-def conn_str() -> str:
-    PASSWORD = "root"
-    PUBLIC_IP_ADDRESS = "35.194.194.150"
-    DBNAME = "newsdb"
-    PROJECT_ID = "cloud-sql-407317"
-    INSTANCE_NAME = "cloudmysql"
-    return f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}?charset=utf8mb4"
 
 
 app = Flask(__name__)
 app.secret_key = '1234567890qwertyuiop'
 app.config['PAGE_SIZE'] = 10
-app.config["SQLALCHEMY_DATABASE_URI"] = conn_str()
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = connect_unix_socket
+# configure Flask-SQLAlchemy to use Python Connector
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://"
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"creator": get_conn}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
