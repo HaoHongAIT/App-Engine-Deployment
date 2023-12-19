1b7ab781265b60117681ceaaf28a2ec0cf789688
hongtienhaolk2001 <hongtienhaolk2001@gmail.com>
2023-12-19 23:19:02 +0700

    231219

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-from flask import request, render_template
-from app import utils, app
+
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, current_user
+
+from app import utils, app, login_manager
 from app.analysis import Analysis
 
 
@@ -35,11 +38,65 @@ def read():
     return render_template('read.html')
 
 
+@app.route('/login', methods=['get', 'post'])
+def user_signin():
+    error_msg = ""
+    if request.method.__eq__('POST'):  # check method
+        username = request.form.get('username')  # get value from form
+        password = request.form.get('password')
+        user = utils.check_login(username, password)  # check  (user, pass) match  between form submit and database
+        if user:
+            login_user(user=user)
+            return redirect(url_for('index'))  # direct to index.html
+        else:
+            error_msg = "Wrong password or username"  # assign variable to show error
+    return render_template('login.html', error_msg=error_msg)
+
+
+@login_manager.user_loader
+def user_load(user_id):
+    return utils.get_user_by_id(user_id=user_id)
+
+
+@app.route('/logout')
+def user_logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['get', 'post'])
+def user_signup():
+    error_msg = ""
+    if request.method.__eq__('POST'):
+        name = str(request.form.get('name')).strip()
+        username = str(request.form.get('username')).strip()
+        password = str(request.form.get('password')).strip()
+        email = str(request.form.get('email')).strip()
+        confirm = str(request.form.get('confirm')).strip()
+        try:
+            if password.__eq__(confirm):
+                utils.add_user(name=name, username=username, password=password, email=email)
+                return redirect(url_for('user_signin'))
+            else:
+                error_msg = "Mật khẩu xác nhận không khớp, vui lòng kiểm tra lại"
+        except Exception as ex:
+            error_msg = "Error: " + str(ex)
+    return render_template('register.html', error_msg=error_msg)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
+
+
 @app.route('/analysis', methods=['GET', 'POST'])
 def analysis():
+    if current_user.is_authenticated:
+        return render_template('login.html')
+
     if request.method.__eq__('POST'):
         news_sentence = f"{request.form['headline']} {request.form['brief']}"  # Get review from input
-        model = Analysis(api_key="your API key")
+        model = Analysis(api_key="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")
         final_output = model.get_response(news_sentence)
         return render_template("analysis.html",
                                predict=final_output)
